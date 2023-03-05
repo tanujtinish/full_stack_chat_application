185357158cf7d32269e4c472ae648921e5d66207
Tanuj Gupta <tanujgupta@tanujs-air.lan>
2023-03-04 22:50:19 -0800

    Added spring boot app for WebSocketChatService

---
+++ Backend/WebSocketChatService/WebSocketChatService/src/test/java/com/tanuj/WebSocketChatService/WebSocketChatServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tanuj.WebSocketChatService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebSocketChatServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
