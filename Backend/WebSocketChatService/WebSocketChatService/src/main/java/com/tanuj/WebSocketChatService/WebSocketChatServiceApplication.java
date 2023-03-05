54f05dd3dd8a0d49bb2f476196d1a624c84c6ea4
tanujtinish <tanujtinish@gmail.com>
2023-03-04 22:50:19 -0800

    Added spring boot app for WebSocketChatService

---
+++ Backend/WebSocketChatService/WebSocketChatService/src/main/java/com/tanuj/WebSocketChatService/WebSocketChatServiceApplication.java
@@ -0,0 +1,13 @@
+package com.tanuj.WebSocketChatService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebSocketChatServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebSocketChatServiceApplication.class, args);
+	}
+
+}
