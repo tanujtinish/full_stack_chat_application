75d2ebaadc425de908fdc289d65f58d46b433d6a
tanujtinish <tanujtinish@gmail.com>
2023-03-05 12:28:13 -0800

    Added Message DTO to be sent and recieved beween frontend and backend app

---
+++ Backend/WebSocketChatService/WebSocketChatService/src/main/java/com/tanuj/WebSocketChatService/model/DTO/MessageDTO.java
@@ -0,0 +1,24 @@
+package main.java.com.tanuj.WebSocketChatService.model.DTO;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import java.util.Date;
+
+import com.tanuj.webSocketManager.model.enums.MessageState;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class MessageDTO {
+
+   private String conversationId;
+   private String messageString;
+   private Date timestamp;
+   private MessageState state;
+   private String senderId;
+   private String recieverId;
+}
