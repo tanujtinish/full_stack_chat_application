c7485826fc2b6886f1e78e92c3cc8415f22b4cae
Tanuj Gupta <tanujgupta@tanujs-air.lan>
2023-03-04 22:52:25 -0800

    Added model(M in MVC) to store chat messages in mongoDb. Added 2 classes to store message and enum to store message state

---
+++ Backend/WebSocketChatService/WebSocketChatService/src/main/java/com/tanuj/WebSocketChatService/model/Message.java
@@ -0,0 +1,25 @@
+package com.tanuj.webSocketManager.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+import java.util.Date;
+
+import com.tanuj.webSocketManager.model.enums.MessageState;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Document
+public class Message {
+   @Id
+   private String id;
+   private String conversationId;
+   private String messageString;
+   private Date timestamp;
+   private MessageState state;
+}
---
+++ Backend/WebSocketChatService/WebSocketChatService/src/main/java/com/tanuj/WebSocketChatService/model/enums/MessageState.java
@@ -0,0 +1,5 @@
+package com.tanuj.webSocketManager.model.enums;
+
+public enum MessageState {
+    RECEIVED, DELIVERED
+}
