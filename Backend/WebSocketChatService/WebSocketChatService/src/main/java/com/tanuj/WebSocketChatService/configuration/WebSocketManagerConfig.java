6b416340e513ed10231a8c18ab7c9dcc29f7f434
tanujtinish <tanujtinish@gmail.com>
2023-03-05 15:24:56 -0800

    Added web socket configuration file

---
+++ Backend/WebSocketChatService/WebSocketChatService/src/main/java/com/tanuj/WebSocketChatService/configuration/WebSocketManagerConfig.java
@@ -0,0 +1,56 @@
+package com.tanuj.WebSocketChatService.configuration;
+
+import java.util.List;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.messaging.converter.DefaultContentTypeResolver;
+import org.springframework.messaging.converter.MappingJackson2MessageConverter;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+import org.springframework.messaging.converter.MessageConverter;
+
+
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketManagerConfig implements WebSocketMessageBrokerConfigurer {
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry config) {
+        
+        config.setApplicationDestinationPrefixes("/user_messages_topic");
+        
+        config.enableSimpleBroker( "/chat_app");
+        
+        config.setUserDestinationPrefix("/user_messages_topic");
+    }
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        
+        registry.addEndpoint("/web_socket").setAllowedOrigins("*").withSockJS();
+    }
+
+    private MessageConverter createJsonMessageConverter() {
+
+        MappingJackson2MessageConverter converter = new MappingJackson2MessageConverter();
+        DefaultContentTypeResolver resolver = new DefaultContentTypeResolver();
+        resolver.setDefaultMimeType(MediaType.APPLICATION_JSON);
+        converter.setContentTypeResolver(resolver);
+        
+        return converter;
+    }
+
+    @Override
+    public boolean configureMessageConverters(List<MessageConverter> converters) {
+
+        converters.add(createJsonMessageConverter());
+
+        return false;
+    }
+}
+
