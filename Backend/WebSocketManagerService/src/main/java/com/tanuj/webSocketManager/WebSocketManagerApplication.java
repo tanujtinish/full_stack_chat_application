258aab566596667fd822abea3e6fad8cd1cb1a54
Tanuj Gupta <tanujgupta@tanujs-air.lan>
2023-03-04 17:52:21 -0800

    Added spring boot app for WebSocketManagerService

---
+++ Backend/WebSocketManagerService/src/main/java/com/tanuj/webSocketManager/WebSocketManagerApplication.java
@@ -0,0 +1,13 @@
+package com.tanuj.webSocketManager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebSocketManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebSocketManagerApplication.class, args);
+	}
+
+}
---
+++ Backend/WebSocketManagerService/src/test/java/com/tanuj/webSocketManager/WebSocketManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tanuj.webSocketManager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebSocketManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
