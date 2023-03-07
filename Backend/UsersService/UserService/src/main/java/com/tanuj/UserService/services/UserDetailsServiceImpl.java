977f902305cd385d4cfd1bf4c37c4df309a096bc
Tanuj Gupta <tanujgupta@eduroam-169-233-177-231.ucsc.edu>
2023-03-07 13:26:32 -0800

    Added implementation for user details service to interact with userDetailsImpl

---
+++ Backend/UsersService/UserService/src/main/java/com/tanuj/UserService/services/UserDetailsServiceImpl.java
@@ -0,0 +1,35 @@
+package com.tanuj.UserService.services;
+
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+import com.tanuj.UserService.model.Users;
+import com.tanuj.UserService.repository.UsersRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.*;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+  
+    @Autowired
+    UsersRepository userRepository;
+
+    @Override
+    @Transactional
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        Optional<Users> user = userRepository.findByUsername(username);
+
+        if(user.isPresent())
+            throw new UsernameNotFoundException("User Not Found with username: " + username);
+
+        UserDetails userDetails = UserDetailsImpl.build(user.get());
+
+        return userDetails;
+    }
+
+}
