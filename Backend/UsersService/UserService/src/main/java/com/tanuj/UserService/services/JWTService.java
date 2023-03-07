98cf84a60eabe98e697e747aa6d6732ecbec5324
Tanuj Gupta <tanujgupta@eduroam-169-233-177-231.ucsc.edu>
2023-03-07 13:31:17 -0800

    Added utility class for JWT. It helps create JWT given user details, and fetch user details given JWT

---
+++ Backend/UsersService/UserService/src/main/java/com/tanuj/UserService/services/JWTService.java
@@ -0,0 +1,114 @@
+package com.tanuj.UserService.services;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.util.WebUtils;
+
+import io.jsonwebtoken.JwtBuilder;
+import io.jsonwebtoken.JwtParser;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.Cookie;
+
+import org.springframework.http.ResponseCookie;
+import org.springframework.http.ResponseCookie.ResponseCookieBuilder;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+@Component
+public class JWTService {
+
+    @Value("${tanuj.chatapp.jwt.secretKey}")
+    private String secretKey;
+
+    @Value("${tanuj.chatapp.jwt.cookie.name}")
+    private String tokenCookieName;
+
+    @Value("${tanuj.chatapp.jwt.expiration}")
+    private int tokenExpiration;
+
+    @Value("${tanuj.chatapp.jwt.maximumAge}")
+    private int tokenMaxAge;
+
+    public String createTokenUsingUsername(String username) {
+        
+        long timeNow = (new Date()).getTime();
+
+        JwtBuilder jwtBuilder = Jwts.builder();
+        jwtBuilder.setExpiration(new Date(timeNow + tokenExpiration));
+        jwtBuilder.setSubject(username);
+        jwtBuilder.setIssuedAt(new Date());
+        jwtBuilder.signWith(SignatureAlgorithm.HS512, secretKey);
+
+        String token = jwtBuilder.compact();
+        
+        return token;
+    }
+    
+    public String extractTokenInCookies(HttpServletRequest request) {
+
+        Cookie tokenCookie = WebUtils.getCookie((HttpServletRequest) request, tokenCookieName);
+
+        return (tokenCookie!=null) ?  tokenCookie.getValue() : null;
+
+    }
+
+    public ResponseCookie createCookieWithTokenFromUsername(UserDetailsImpl principal) {
+
+        String token = createTokenUsingUsername(principal.getUsername());
+
+        ResponseCookieBuilder tokenCookieBuilder = ResponseCookie.from(tokenCookieName, token);
+        tokenCookieBuilder.maxAge(tokenMaxAge);
+        tokenCookieBuilder.path("/security");
+        tokenCookieBuilder.httpOnly(true);
+
+        ResponseCookie tokenCookie = tokenCookieBuilder.build();
+
+        return tokenCookie;
+    }
+
+    public ResponseCookie createCookieWithNullToken() {
+
+            ResponseCookieBuilder tokenCookieBuilder = ResponseCookie.from(tokenCookieName, null);
+            tokenCookieBuilder.path("/security");
+            
+            ResponseCookie tokenCookie = tokenCookieBuilder.build();
+
+            return tokenCookie;
+    }
+
+    public String getUserNameFromJwtToken(String token) {
+
+        try {
+            JwtParser jwtParser = Jwts.parser();
+            jwtParser.setSigningKey(secretKey);
+
+            return jwtParser.parseClaimsJws(token).getBody().getSubject();
+
+        } catch (Exception e) {
+            return null;
+        }
+
+    }
+
+    public boolean validateToken(String token) {
+
+        try {
+
+            JwtParser jwtParser = Jwts.parser();
+
+            jwtParser.setSigningKey(secretKey);
+            jwtParser.parseClaimsJws(token);
+            
+            return true;
+
+        } catch (Exception e) {
+
+        }
+
+        return false;
+    }
+  
+  
+}
