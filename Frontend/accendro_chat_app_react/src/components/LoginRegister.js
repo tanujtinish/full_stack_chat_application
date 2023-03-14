eb8a78e44a206a70a0f039a2cb240848e08cc56f
Tanuj Gupta <tanujgupta@tanujs-air.lan>
2023-03-13 19:57:11 -0700

    Added parent component for login and sinup transition. Also added css for it

---
+++ Frontend/accendro_chat_app_react/src/components/LoginRegister.js
@@ -0,0 +1,62 @@
+import React, { useState, useEffect } from "react";
+
+import { useSelector } from "react-redux";
+
+import Register from "./Register"
+import Login from "./Login"
+
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Navigate  } from 'react-router-dom';
+
+import css from "../css/LoginRegister.css";
+
+const LoginRegister = (props) => {
+
+    const [isRightPanelActive, setIsRightPanelActive] = useState(false);
+    const classNames =  `${isRightPanelActive ? 'container right-panel-active' : 'container'}`;
+
+    // useEffect(() => {
+    //     console.log("LoginRegister rerenders");
+    // });
+
+    const {isLoggedIn: isLoggedIn } = useSelector(state => state.UserServiceReducer);
+
+    const handleSignUp = (e) => {
+        e.preventDefault();
+        
+        setIsRightPanelActive(true);
+    }
+
+    const handleSignIn = (e) => {
+        e.preventDefault();
+        
+        setIsRightPanelActive(false);
+    }
+
+    if (isLoggedIn) {
+        return <Navigate to="/profile" />;
+    }
+    
+    return (
+        <div className={classNames} id="container">
+            <Login/>
+            <Register/>
+            <div className="overlay-container">
+                <div class="overlay">
+                <div class="overlay-panel overlay-left">
+                    <h1>Welcome Back!</h1>
+                    <p>To keep connected with us please login with your personal info</p>
+                    <button class="ghost" id="signIn" onClick={handleSignIn}>Sign In</button>
+                </div>
+                <div class="overlay-panel overlay-right">
+                    <h1>Hello, Friend!</h1>
+                    <p>Enter your personal details and start journey with us</p>
+                    <button class="ghost" id="signUp" onClick={handleSignUp}>Sign Up</button>
+                </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default LoginRegister;
