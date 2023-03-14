12e71241f2e2be8310d4a924cc061afdeb762b5b
Tanuj Gupta <tanujgupta@tanujs-air.lan>
2023-03-13 19:48:19 -0700

    Created basic layout for Parent App component in react

--- Frontend/accendro_chat_app_react/src/App.js
+++ Frontend/accendro_chat_app_react/src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Routes, Route } from "react-router-dom";
 
-function App() {
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+
+
+
+const App = () => {
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+
+      {/* <Header/> */}
+
+      <div className="container mt-3">
+        <Routes>
+         
+        </Routes>
+      </div>
+
+      {/* <AuthVerify logOut={logOut}/> */}
     </div>
   );
-}
+};
 
 export default App;
