184f51765f3fd044380329017b18f65fabc85392
Tanuj Gupta <tanujgupta@tanujs-air.lan>
2023-03-13 19:52:12 -0700

    Added redux action for user service

---
+++ Frontend/accendro_chat_app_react/src/actions/user_service_action_types.js
@@ -0,0 +1,10 @@
+export const SIGN_UP_SUCCESS = "SIGN_UP_SUCCESS";
+export const SIGN_UP_FAIL = "SIGN_UP_FAIL";
+
+export const LOGIN_SUCCESS = "LOGIN_SUCCESS";
+export const LOGIN_FAIL = "LOGIN_FAIL";
+
+export const LOGOUT_SUCCESS = "LOGOUT_SUCCESS";
+export const LOGOUT_FAIL = "LOGOUT_FAIL";
+
+export const UNSET_MESSAGE = "UNSET_MESSAGE";
