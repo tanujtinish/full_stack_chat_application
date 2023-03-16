a6b37f236c65301841c45fcac2c6c58e0766b70c
Tanuj Gupta <tanujgupta@eduroam-169-233-205-25.ucsc.edu>
2023-03-15 21:45:18 -0700

    Removed getUsers state from redux and use it in component

--- Frontend/accendro_chat_app_react/src/actions/user_service_action_types.js
+++ Frontend/accendro_chat_app_react/src/actions/user_service_action_types.js
@@ -3,7 +3,4 @@ export const LOGIN_SUCCESS = "LOGIN_SUCCESS";
 export const LOGOUT_SUCCESS = "LOGOUT_SUCCESS";
 export const LOGOUT_FAIL = "LOGOUT_FAIL";
 
-export const GET_USERS_SUCCESS = "GET_USERS_SUCCESS";
-export const GET_USERS_FAIL = "GET_USERS_FAIL";
-
 export const UNSET_MESSAGE = "UNSET_MESSAGE";
