fe475598f1025e8021b876074468bc8f848e96cb
Tanuj Gupta <tanujgupta@tanujs-air.lan>
2023-03-19 10:33:53 -0700

    Added service class for AuthenticationService and restructured code for signUpUser to use service and offload task to service class from controller class

---
+++ Backend/UsersService/UserService/src/test/java/com/tanuj/UserService/controllers/AuthenticationControllerIntegrationTest.java
@@ -0,0 +1,25 @@
+// package com.tanuj.UserService.controllers;
+
+// import org.junit.runner.RunWith;
+// import org.springframework.boot.test.context.SpringBootTest;
+// import org.springframework.test.context.junit4.SpringRunner;
+
+// @RunWith(SpringRunner.class)
+// @SpringBootTest
+// public class AuthenticationControllerIntegrationTest {
+    
+//     @Test
+//     public void givenEmployees_whenGetEmployees_thenStatus200()
+//     throws Exception {
+
+//         createTestEmployee("bob");
+
+//         mvc.perform(get("/api/employees")
+//         .contentType(MediaType.APPLICATION_JSON))
+//         .andExpect(status().isOk())
+//         .andExpect(content()
+//         .contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+//         .andExpect(jsonPath("$[0].name", is("bob")));
+//     }
+
+// }
