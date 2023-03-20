9f9bd79d8b1df5104593649f802458e9a6ae60a7
Tanuj Gupta <tanujgupta@tanujs-air.lan>
2023-03-19 17:38:36 -0700

    Added json creator and overrided equals in MessageDTO class to convert api response to MessageDTO using object mapper

--- Backend/UsersService/UserService/src/main/java/com/tanuj/UserService/model/DTO/MessageDTO.java
+++ Backend/UsersService/UserService/src/main/java/com/tanuj/UserService/model/DTO/MessageDTO.java
@@ -1,5 +1,8 @@
 package com.tanuj.UserService.model.DTO;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import lombok.*;
 
 @Getter
@@ -8,8 +11,24 @@ public class MessageDTO {
     
     private String message;
 
-    public MessageDTO(String message) {
-    this.message = message;
+    @JsonCreator
+    public MessageDTO(@JsonProperty("message") String message) {
+        this.message = message;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == this) {
+            return true;
+        }
+
+        if (!(obj instanceof MessageDTO)) {
+            return false;
+        }
+
+        MessageDTO other = (MessageDTO) obj;
+
+        return (other.message.compareTo(this.message) == 0);
     }
 
 }
