65e6070409a752f273d3250068a7b82d54f081d2
tanujtinish <tanujtinish@gmail.com>
2023-03-20 22:29:07 -0700

    Websocket service closing its feign client role

--- Backend/WebSocketChatService/WebSocketChatService/src/main/java/com/tanuj/WebSocketChatService/service/UserServiceFeignClient.java
+++ Backend/WebSocketChatService/WebSocketChatService/src/main/java/com/tanuj/WebSocketChatService/service/UserServiceFeignClient.java
@@ -1,12 +1,15 @@
 package com.tanuj.WebSocketChatService.service;
 
-import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@FeignClient("chat-app-user-service")
-public interface UserServiceFeignClient {
+// @FeignClient("chat-app-user-service")
+@Service
+public class UserServiceFeignClient {
 
     @RequestMapping("/chat/user/users/{userId}")
-    String findAll();
+    public String findAll(){
+        return "";
+    };
 
 }
