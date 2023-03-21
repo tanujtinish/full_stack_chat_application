15757f65b14dc4f9e1effae0890f78110eb4468a
Tanuj Gupta <tanujgupta@tanujs-air.lan>
2023-03-20 18:55:25 -0700

    Made web socket service feign client to use userService client

--- Backend/WebSocketChatService/WebSocketChatService/src/main/java/com/tanuj/WebSocketChatService/WebSocketChatServiceApplication.java
+++ Backend/WebSocketChatService/WebSocketChatService/src/main/java/com/tanuj/WebSocketChatService/WebSocketChatServiceApplication.java
@@ -2,8 +2,10 @@ package com.tanuj.WebSocketChatService;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class WebSocketChatServiceApplication {
 
 	public static void main(String[] args) {
