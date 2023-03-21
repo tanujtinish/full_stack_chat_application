b95b85c3a4fcd823e7498219d04fbbd5dd87aebe
tanujtinish <tanujtinish@gmail.com>
2023-03-20 18:38:09 -0700

    Updated root docker compose file context for eureka service

--- Backend/UsersService/UserService/src/main/java/com/tanuj/UserService/UserServiceApplication.java
+++ Backend/UsersService/UserService/src/main/java/com/tanuj/UserService/UserServiceApplication.java
@@ -2,8 +2,9 @@ package com.tanuj.UserService;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 @SpringBootApplication
+@EnableDiscoveryClient
 public class UserServiceApplication {
 
 	public static void main(String[] args) {
