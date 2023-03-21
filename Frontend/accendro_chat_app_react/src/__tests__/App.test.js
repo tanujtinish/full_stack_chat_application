a2531ee842475ca1a31f9b237132c9d0c3a51de5
tanujtinish <tanujtinish@gmail.com>
2023-03-20 18:39:07 -0700

    Added react app integration test for APP

--- Frontend/accendro_chat_app_react/src/__tests__/App.test.js
+++ Frontend/accendro_chat_app_react/src/__tests__/App.test.js
@@ -1,25 +1,23 @@
 import React from 'react';
-import { render, screen, fireEvent, act, getByTestId } from '@testing-library/react';
-import Chat from '../Chat';
+import { render, screen, act } from '@testing-library/react';
+import App from '../App';
 import { BrowserRouter } from "react-router-dom";
-import axios from 'axios';
 import { Provider } from 'react-redux';
 import configureStore from 'redux-mock-store';
 import '@testing-library/jest-dom';
-import { Routes, Route } from "react-router-dom";
-import { count_new_messgaes_api_call } from '../../Utils/ChatServiveApiUtils';
-import { get_users_api_call } from '../../Utils/UserServiceApiUtils';
-import { get_messages_api_call } from '../../Utils/ChatServiveApiUtils';
-import { create_connection_to_stomp_server } from '../../Utils/ChatServiveApiUtils';
-import { send_message_to_stomp_server } from '../../Utils/ChatServiveApiUtils';
+import { count_new_messgaes_api_call } from '../Utils/ChatServiveApiUtils';
+import { get_users_api_call } from '../Utils/UserServiceApiUtils';
+import { get_messages_api_call } from '../Utils/ChatServiveApiUtils';
+import { create_connection_to_stomp_server } from '../Utils/ChatServiveApiUtils';
+import { send_message_to_stomp_server } from '../Utils/ChatServiveApiUtils';
 
 const mockStore = configureStore([]);
 
-jest.mock('../../Utils/UserServiceApiUtils.js', () => ({
+jest.mock('../Utils/UserServiceApiUtils.js', () => ({
     get_users_api_call: jest.fn(),
 }));
 
-jest.mock('../../Utils/ChatServiveApiUtils.js', () => ({
+jest.mock('../Utils/ChatServiveApiUtils.js', () => ({
     __esModule: true,
     count_new_messgaes_api_call: jest.fn(),
     get_messages_api_call: jest.fn(),
@@ -31,9 +29,6 @@ describe('Chat', () => {
     let store;
 
     beforeEach(() => {
-        store = mockStore({
-            UserServiceReducer: { isLoggedIn: true, userInfo: {id:10, username:"test username 10"}, jwt_web_token:"test token", logout_message: ""},
-        });
 
         get_users_api_call.mockReturnValue(
             new Promise((resolve, reject) => {
@@ -57,33 +52,11 @@ describe('Chat', () => {
         create_connection_to_stomp_server.mockReturnValue();
     });
 
-    it('Chat should update messages state on send button', async () => {
+    it('App should show loginRegister component if user not loggerin', async () => {
 
-        let component;
-        await act(async () => { 
-            // const { getByTestId, getByRole, queryAllByRole, debug, container } 
-            component =
-            render(
-                <Provider store={store}>
-                    <PersistGate loading={null} persistor={persistor}>
-                        <BrowserRouter>
-                            <App />
-                        </BrowserRouter>
-                    </PersistGate>
-                </Provider>
-            )}
-        );
-
-        const inputElement = screen.getByPlaceholderText('Write your message...');
-        fireEvent.change(inputElement, { target: { value: 'hello' } });
-        const buttonElement = screen.getByTestId('send-message-button');
-        fireEvent.click(buttonElement);
-        const messageElement = screen.queryByText('hello');
-        expect(messageElement).toBeInTheDocument();
-    });
-
-
-    it('Chat should render profile comonent only after profile button click', async () => {
+        store = mockStore({
+            UserServiceReducer: { isLoggedIn: false, userInfo: {id:-1, username:""}, jwt_web_token:"", logout_message: ""},
+        });
 
         let component;
         await act(async () => { 
@@ -92,23 +65,44 @@ describe('Chat', () => {
             render(
                 <Provider store={store}>
                     <BrowserRouter>
-                        <Routes>
-                            <Route path="/" element={<Chat />} />
-                            <Route path="/login_register" element={<div></div>} />
-                        </Routes>
+                        <App />
                     </BrowserRouter>
-                </Provider> 
+                </Provider>
             )}
         );
 
-        const profileComponentBeforeClick = screen.queryByTestId('profile-component');
-        expect(profileComponentBeforeClick).toEqual(null);
+        const chatComponent = screen.queryByTestId('chat-component');
+        expect(chatComponent).toEqual(null);
 
-        const buttonElement = screen.getByTestId('show-profile-button');
-        fireEvent.click(buttonElement);
-        
-        const profileComponentAfterClick = screen.queryByTestId('profile-component');
-        expect(profileComponentAfterClick).toBeInTheDocument();
+        const loginRegisterComponent = screen.queryByTestId('LoginRegister-component');
+        expect(loginRegisterComponent).toBeInTheDocument();
+    });
+
+
+    it('App should show Chat component if user loggerin', async () => {
+
+        store = mockStore({
+            UserServiceReducer: { isLoggedIn: true, userInfo: {id:10, username:"test username 10"}, jwt_web_token:"test token", logout_message: ""},
+        });
+
+        let component;
+        await act(async () => { 
+            // const { getByTestId, getByRole, queryAllByRole, debug, container } 
+            component =
+            render(
+                <Provider store={store}>
+                    <BrowserRouter>
+                        <App />
+                    </BrowserRouter>
+                </Provider>
+            )}
+        );
+
+        const chatComponent = screen.queryByTestId('chat-component');
+        expect(chatComponent).toBeInTheDocument();
+
+        const loginRegisterComponent = screen.queryByTestId('LoginRegister-component');
+        expect(loginRegisterComponent).toEqual(null);
     });
 
 });
